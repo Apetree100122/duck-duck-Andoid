c2cfde0c4017433c90418af6abae24c9e7c35cf1
Lukasz Macionczyk <lukasz.macionczyk@gmail.com>
2023-11-24 11:28:19 +0100

    trigger popup on refreshing the page

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
@@ -1108,11 +1108,11 @@ class BrowserTabFragment :
     }
 
     private fun onRefreshRequested() {
-        viewModel.onRefreshRequested()
+        viewModel.onRefreshRequested(triggeredByUser = true)
     }
 
     override fun onAutofillStateChange() {
-        viewModel.onRefreshRequested()
+        viewModel.onRefreshRequested(triggeredByUser = false)
     }
 
     override fun onRejectGeneratedPassword(originalUrl: String) {
@@ -3192,7 +3192,7 @@ class BrowserTabFragment :
                     viewModel.onUserLongPressedBack()
                 }
                 onMenuItemClicked(menuBinding.refreshMenuItem) {
-                    viewModel.onRefreshRequested()
+                    viewModel.onRefreshRequested(triggeredByUser = true)
                     pixel.fire(AppPixelName.MENU_ACTION_REFRESH_PRESSED.pixelName)
                 }
                 onMenuItemClicked(menuBinding.newTabMenuItem) {
